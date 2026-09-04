27ba22cb390bb0f3c7661cab5de9ee7b43aa4211
hernandes244 <Wolfman13690@gmail.com>
2026-09-03 21:48:03 -0300

    feat: route mobile WMS interface

--- wms/urls.py
+++ wms/urls.py
@@ -1,3 +1,3 @@
 from django.urls import path
-from .views import dashboard,stock,receipts,orders,pickings,movements
-urlpatterns=[path('',dashboard,name='dashboard'),path('estoque/',stock,name='stock'),path('entradas/',receipts,name='receipts'),path('pedidos/',orders,name='orders'),path('separacoes/',pickings,name='pickings'),path('movimentacoes/',movements,name='movements')]
+from .views import dashboard,mobile,stock,receipts,orders,pickings,movements
+urlpatterns=[path('',dashboard,name='dashboard'),path('mobile/',mobile,name='mobile'),path('estoque/',stock,name='stock'),path('entradas/',receipts,name='receipts'),path('pedidos/',orders,name='orders'),path('separacoes/',pickings,name='pickings'),path('movimentacoes/',movements,name='movements')]
